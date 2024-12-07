c40542355165ae14405cf0ded29c34d6f6d3b6f0
leonl42 <noinnick8@gmail.com>
2024-12-07 12:30:03 +0100

    update gitignore

--- part1/run.sh
+++ part1/run.sh
@@ -1,3 +1,3 @@
-python main.py ./exps_adam/no_wd_lr0.0001/ --reset --bfloat16
-python main.py ./exps_adam/wd0.0015_lr0.0001/ --reset --bfloat16
+python main.py ./exps_adam/no_wd_lr0.0001/
+python main.py ./exps_adam/wd0.0015_lr0.0001/
 
